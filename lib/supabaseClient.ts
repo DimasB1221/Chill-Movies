948ab2fe4ecb6d340dc90419b8774506377010d8
Dimas Bintang <dimasbintang1818@gmail.com>
2026-01-21 20:46:40 +0700

    minor changes

--- lib/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = process.env.VITE_SUPA_URL;
-export const supabaseAnonKey = process.env.VITE_SUPA_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Supabase URL atau Anon Key belum disetting di .env");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
