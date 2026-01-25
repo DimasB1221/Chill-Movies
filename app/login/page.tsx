574d106dedbb0d36d512b4ee4f65dcc3fe7d6e85
Dimas Bintang <dimasbintang1818@gmail.com>
2026-01-25 10:40:27 +0700

    feat : add cardRegisterLogin component

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import CardRegisterLogin from "@/components/ui/cardRegisterLogin";
+
+function Login() {
+  return (
+    <main className="flex min-h-screen items-center px-2 justify-center bg-login ">
+      <CardRegisterLogin />
+    </main>
+  );
+}
+
+export default Login;
---
+++ app/register/page.tsx
@@ -0,0 +1,11 @@
+import CardRegisterLogin from "@/components/ui/cardRegisterLogin";
+
+function Register() {
+  return (
+    <main className="flex min-h-screen px-2 justify-center items-center bg-register">
+      <CardRegisterLogin />
+    </main>
+  );
+}
+
+export default Register;
