99fa22807dd298e86734e06fa6963dcaf0860683
Dimas Bintang <dimasbintang1818@gmail.com>
2026-02-19 09:23:48 +0700

    feat : setup tanstack

---
+++ src/app/provider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import * as React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { MainErrorFallback } from "@/src/components/errors/main";
+
+import { queryConfig } from "@/src/lib/reactQuery";
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: queryConfig,
+      }),
+  );
+
+  return (
+    <ErrorBoundary FallbackComponent={MainErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        {process.env.DEV && <ReactQueryDevtools />}
+        {children}
+      </QueryClientProvider>
+    </ErrorBoundary>
+  );
+};
